24c2a607bffcdba0a48d5eaa0ad94af9fa96f53d
Alvarotloc <alvarotloc@gmail.com>
2022-11-29 15:15:51 +0100

    Creadas las paginas de registro y recuperar password

--- src/pages/OlvidePassword.tsx
+++ src/pages/OlvidePassword.tsx
@@ -1,6 +1,50 @@
-import { FC } from 'react';
-export const OlvidePassword:FC = ():JSX.Element => {
+import { FC } from "react";
+import { Link } from "react-router-dom";
+export const OlvidePassword: FC = (): JSX.Element => {
   return (
-    <div>OlvidePassword</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-5xl sm:text-6xl  capitalize">
+        Recupera tu acceso y administra{" "}
+        <span className="text-slate-700">tus proyectos</span>
+      </h1>
+      <form className="mt-10 mb-5 shadow bg-white rounded-md p-10">
+        <fieldset>
+          <div className="mb-5 space-y-2">
+            <label
+              htmlFor="email"
+              className="uppercase text-gray-600 font-bold block"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email de registro"
+              className="w-full px-3 py-2 border rounded-md bg-gray-50"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Enviar Instrucciones"
+            className="bg-sky-700 w-full rounded-md py-3 text-white font-bold uppercase hover:bg-sky-800 transition-colors cursor-pointer"
+          />
+        </fieldset>
+      </form>
+      <nav className="flex flex-col gap-5 lg:gap-0 lg:flex-row lg:justify-between">
+        <Link
+          to="/registrar"
+          className="text-sm lg:text-xs hover:underline text-center text-slate-500"
+        >
+          ¿No tienes cuenta? Crea una
+        </Link>
+        <Link
+          to="/"
+          className="text-sm lg:text-xs hover:underline text-center text-slate-500"
+        >
+          ¿Ya tienes una cuenta? Inicia sesión
+        </Link>
+      </nav>
+    </>
+  );
+};
--- src/pages/Registrar.tsx
+++ src/pages/Registrar.tsx
@@ -1,6 +1,95 @@
-import { FC } from 'react';
-export const Registrar:FC = ():JSX.Element => {
+import { FC } from "react";
+import { Link } from "react-router-dom";
+export const Registrar: FC = (): JSX.Element => {
   return (
-    <div>Registrar</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-5xl sm:text-6xl capitalize">
+        Crea tu cuenta y administra{" "}
+        <span className="text-slate-700">tus proyectos</span>
+      </h1>
+      <form className="mt-10 mb-5 shadow bg-white rounded-md p-10">
+        <fieldset>
+          <div className="mb-5 space-y-2">
+            <label
+              htmlFor="nombre"
+              className="uppercase text-gray-600 font-bold block"
+            >
+              Nombre
+            </label>
+            <input
+              type="text"
+              name="nombre"
+              id="nombre"
+              placeholder="Tu nombre"
+              className="w-full px-3 py-1 border rounded-md bg-gray-50"
+            />
+          </div>
+          <div className="mb-5 space-y-2">
+            <label
+              htmlFor="email"
+              className="uppercase text-gray-600 font-bold block"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email de registro"
+              className="w-full px-3 py-1 border rounded-md bg-gray-50"
+            />
+          </div>
+          <div className="my-5 space-y-2">
+            <label
+              htmlFor="password"
+              className="uppercase text-gray-600 font-bold block"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password de registro"
+              className="w-full px-3 py-1 border rounded-md bg-gray-50"
+            />
+          </div>
+          <div className="my-5 space-y-2">
+            <label
+              htmlFor="confirmar"
+              className="uppercase text-gray-600 font-bold block"
+            >
+              Confirmar Contraseña
+            </label>
+            <input
+              type="password"
+              name="confirmar"
+              id="confirmar"
+              placeholder="Confirma tu contraseña"
+              className="w-full px-3 py-1 border rounded-md bg-gray-50"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Crear Cuenta"
+            className="bg-sky-700 w-full rounded-md py-3 text-white font-bold uppercase hover:bg-sky-800 transition-colors cursor-pointer"
+          />
+        </fieldset>
+      </form>
+      <nav className="flex flex-col gap-5 lg:gap-0 lg:flex-row lg:justify-between">
+        <Link
+          to="/"
+          className="text-sm lg:text-xs hover:underline text-center text-slate-500"
+        >
+          ¿Ya tienes cuenta? Inicia Sesión
+        </Link>
+        <Link
+          to="/olvide-password"
+          className="text-sm lg:text-xs hover:underline text-center text-slate-500"
+        >
+          ¿Has olvidado tu contraseña? Recupérala
+        </Link>
+      </nav>
+    </>
+  );
+};
